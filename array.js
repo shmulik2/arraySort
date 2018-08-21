da90d372d05848c0e858921f79e09e9b449e55c7
shmulik2 <42575439+shmulik2@users.noreply.github.com>
2018-08-21 19:47:34 +0300

    Add files via upload

--- array.js
+++ array.js
@@ -15,15 +15,38 @@ var library = [
         "author": "Suzanne Collins",
         "libraryID": 3245
     }];
+for(var i = 0; i < library.length; i++)	
+{
+sorted = Object.keys(library[i])
+    .sort()
+    .reduce(function (acc, key) { 
+        acc[key] = library[i][key];
+        return acc;
+    }, {});
+	library[i] = sorted;
+}
+	
+	//console.log(library);
  
- function sortByID(array, key) {
- return array.sort(function(a, b) {
+ //var str = '[';
+ //for (var i = 0; i<library.length; ++i) {
+	//str += '{' + "author: " + library[i]["author"] + " " + "libraryID: " + library[i]["libraryID"] + " " + "title: " + library[i]["title"] +// '}';
+ //}
+//str += ']'
+
+ //console.log(str);
+ 
+ 
+  
+  function sortByID(array, key) {
+  return array.sort(function(a, b) {
      var x = b[key]; var y = a[key];
-     return ((x < y) ? -1 : ((x > y) ? 1 : 0));
- });
- }
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+  }
  
- lbr = sortByID(library, 'libraryID');
- 
- console.log(lbr);
- 
+  lbr = sortByID(library, 'libraryID');
+  
+
+
+console.log(lbr);
